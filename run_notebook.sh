c3f6a06ca05dd3450fe2cabc804e64f2c9e9213a
raul-sanchez-martin <r.sanchez.martin.rsm@gmail.com>
2018-08-15 23:57:16 +0200

    add chapter-going-beyond-scala, R

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 8888:8888 -v "$PWD":/home/jovyan/work jupyter/all-spark-notebook
+docker run -it -p 8888:8888 -v "$PWD":/home/jovyan/work jupyter/all-spark-notebook:latest
